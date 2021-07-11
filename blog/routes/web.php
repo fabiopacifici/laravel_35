97b07f825bb2d6a91c89e172bac553773fe4782e
fabiopacifici <mail@fabiopacifici.com>
2021-07-11 20:00:22 +0100

    add dc-comics routes

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -14,27 +14,55 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    // passa variabili alla view con compact()
-    //$name = "Valerio";
-    //return view('home', compact('name'));
 
-    $data = [
-        'name' => 'Classe',
-        'lastname' => '35',
-        'students' => [
-            'Eveling',
-            'Simone',
-            'Andrei',
-            'Stefania',
-            'Beatrice',
-            'Carmen',
-            'Alessandra'
-        ]
-    ];
+    return view('home');
+})->name('home');
 
-    return view('home', $data);
-});
-Route::get('/about', function () {
 
-    return 'About me';
-});
+Route::get('/characters', function () {
+    return 'characters Page';
+})->name('characters');
+
+
+/* Comics routes */
+
+Route::get('/comics', function () {
+    $comics = config('comics.data');
+    //ddd($comics);
+    return view('comics.index', compact('comics'));
+})->name('comics');
+
+Route::get('comics/{id}', function ($id) {
+    $comics = config('comics.data');
+    //ddd($comics[$id]);
+    if (is_numeric($id) && $id < count($comics) && $id >= 0) {
+        $comic = $comics[$id];
+        return view('comics.show', compact('comic'));
+    } else {
+        abort(404);
+    }
+})->name('comic');
+
+/* Comics routes */
+
+
+Route::get('/movies', function () {
+    return 'Movies Page';
+})->name('movies');
+Route::get('/tv', function () {
+
+    return 'TV Page';
+})->name('tv');
+Route::get('/games', function () {
+
+    return 'Games Page';
+})->name('games');
+
+Route::get('/news', function () {
+
+    return 'News Page';
+})->name('news');
+
+Route::get('/shop', function () {
+    return 'Shop Page';
+})->name('shop');
